e288367c193046a4a078eec1804caa4b325aca79
Matthew Krass <mkrass@fmbair016.lan>
2023-06-10 20:26:42 -0400

    just a test

--- prework_assignment.py
+++ prework_assignment.py
@@ -49,7 +49,8 @@ def is_consecutive(a_list):
 print(is_consecutive(a_list = [2,3,4,5,6,7]))
 print(is_consecutive(a_list = (1,2,4,5)))
 print(is_consecutive(a_list = (1,3,5,7,2,4,6)))
-print(is_consecutive(a_list = [1]))
+
+
 
 
 
